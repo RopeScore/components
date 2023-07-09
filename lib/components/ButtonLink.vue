40053b592a876a71f69b00959422f0b3572c4539
Svante Bengtson <svante@swantzter.se>
2023-07-10 01:33:07 +0200

    attempt externalising vue-router

--- lib/components/ButtonLink.vue
+++ lib/components/ButtonLink.vue
@@ -6,15 +6,15 @@ defineProps({
   disabled: TextButton.props.disabled,
   selected: TextButton.props.selected,
   color: TextButton.props.color,
-  // @ts-expect-error this one is global
+  // @ts-expect-error is global
   ...RouterLink.props
 })
 </script>
 
 <template>
   <router-link v-slot="{ href, navigate, isActive }" v-bind="$props" custom>
-    <a :href="href" @click="navigate">
-      <text-button :dense="dense" :selected="selected" :disabled="disabled" @click="navigate">
+    <a :href="href" @click="disabled ? null : navigate()">
+      <text-button :dense="dense" :selected="selected" :disabled="disabled">
         <slot :is-active="isActive" />
       </text-button>
     </a>
